7616906f93d98ddd377134466a12788bfeb08bbb
Marcus Crestani <marcus@crestani.de>
2020-06-24 12:52:50 +0200

    Make `influxdb-stream` more configurable via `opts-map` and use future to connect to influxdb.

--- src/active_riemann/influxdb.clj
+++ src/active_riemann/influxdb.clj
@@ -3,20 +3,44 @@
   (:require [riemann.config :as riemann-config]
             [riemann.influxdb :as riemann-influxdb]
             [riemann.streams :as riemann-streams]
-            [riemann.test :as riemann-test]))
+            [riemann.test :as riemann-test]
+            [clojure.tools.logging :as logging]))
+
+(defn make-influxdb-connection
+  [opts]
+  (try (let [influxdb (riemann-influxdb/influxdb opts)]
+         (logging/info "Connected to influxdb " opts)
+         influxdb)
+       (catch Exception e
+         (logging/warn "Caught event when creating influxdb connection " opts ":" (.getMessage e) ", trying again in 3s.")
+         (Thread/sleep 3000)
+         (make-influxdb-connection opts))))
+
+(defn discard-events
+  [e]
+  (logging/debug "Connection to influxdb not ready, discarding " (count (:event e)) " events"))
 
 (defn make-influxdb-stream
-  [influxdb-host & [db-name tag-fields]]
-  (riemann-streams/smap #(dissoc % :ttl)
-                        (riemann-test/io
-                         (riemann-streams/batch 100 1/10
-                                                (riemann-config/async-queue! (str ::influx "-" influxdb-host "-" db-name)
-                                                                             {:queue-size 10000
-                                                                              :core-pool-size 1
-                                                                              :max-pool-size 128
-                                                                              :keep-alive-time 60000}
-                                                                             (riemann-influxdb/influxdb (merge
-                                                                                                         {:version :0.9
-                                                                                                          :host influxdb-host}
-                                                                                                         (if db-name {:db db-name} {})
-                                                                                                         (if tag-fields {:tag-fields tag-fields} {}))))))))
+  [influxdb-host & [db-name tag-fields opts-map]]
+  (let [{:keys [batch-n batch-dt queue-size core-pool-size max-pool-size keep-alive-time]
+         :or {batch-n 1000 batch-dt 1
+              queue-size 10000 core-pool-size 1
+              max-pool-size 128 keep-alive-time 60000}} opts-map
+        influxdb-future (future (make-influxdb-connection (merge
+                                                           {:version :0.9
+                                                            :host influxdb-host}
+                                                           (if db-name {:db db-name} {})
+                                                           (if tag-fields {:tag-fields tag-fields} {}))))]
+    (riemann-streams/smap #(dissoc % :ttl)
+                          (riemann-test/tap :influxdb
+                                            (riemann-test/io
+                                             (riemann-streams/batch batch-n batch-dt
+                                                                    (riemann-config/async-queue! (str ::influx "-" influxdb-host "-" (or db-name "riemann"))
+                                                                                                 {:queue-size queue-size
+                                                                                                  :core-pool-size core-pool-size
+                                                                                                  :max-pool-size max-pool-size
+                                                                                                  :keep-alive-time keep-alive-time}
+                                                                                                 (fn [e]
+                                                                                                   (if (realized? influxdb-future)
+                                                                                                     (@influxdb-future e)
+                                                                                                     (discard-events e))))))))))
