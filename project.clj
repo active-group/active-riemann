6b0b8ea3b7cbd2d656c2e4ddf0ccb5aeb30c454d
Marcus Crestani <marcus@crestani.de>
2020-08-26 08:25:40 +0200

    Add url to `project.clj`.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject de.active-group/active-riemann "0.5.0-SNAPSHOT"
   :description "Common functionality for Riemann streams."
+  :url "https://github.com/active-group/active-riemann"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
