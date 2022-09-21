cc9ac5a48ed87d7767085514fd0bc475c8ab9100
Marcus Crestani <marcus.crestani@active-group.de>
2022-09-21 13:15:14 +0200

    Add hostname to tests to unbreak breaker tests.

--- test/active_riemann/breaker_test.clj
+++ test/active_riemann/breaker_test.clj
@@ -1,5 +1,6 @@
 (ns active-riemann.breaker-test
   (:require [active-riemann.breaker :refer :all]
+            [active-riemann.common :as common]
             [riemann.streams :as riemann-streams]
             [riemann.test :as riemann-test]
             [clojure.test :as test]))
@@ -8,57 +9,57 @@
   (test/testing "One event with high metric."
     (riemann-test/test-stream
      (load-indicator "test-service" 10 60 (fn [res] (fn [_metric-event] (test/is (true? res)))))
-     [{:service "test-service" :metric 100}]
-     [{:service "test-service" :metric 100}]))
+     [{:service "test-service" :host common/hostname :metric 100}]
+     [{:service "test-service" :host common/hostname :metric 100}]))
   (test/testing "One event with low metric."
     (riemann-test/test-stream
      (load-indicator "test-service" 10 60 (fn [res] (fn [_metric-event] (test/is (false? res)))))
-     [{:service "test-service" :metric 1}]
-     [{:service "test-service" :metric 1}]))
+     [{:service "test-service" :host common/hostname :metric 1}]
+     [{:service "test-service" :host common/hostname :metric 1}]))
   (test/testing "Many events with high metric."
     (riemann-test/test-stream
      (load-indicator "test-service" 10 60 (fn [res] (fn [_metric-event] (test/is (true? res)))))
-     [{:service "test-service" :metric 100}
-      {:service "test-service" :metric 100}
-      {:service "test-service" :metric 10}]
-     [{:service "test-service" :metric 100}
-      {:service "test-service" :metric 100}
-      {:service "test-service" :metric 70}]))
+     [{:service "test-service" :host common/hostname :metric 100}
+      {:service "test-service" :host common/hostname :metric 100}
+      {:service "test-service" :host common/hostname :metric 10}]
+     [{:service "test-service" :host common/hostname :metric 100}
+      {:service "test-service" :host common/hostname :metric 100}
+      {:service "test-service" :host common/hostname :metric 70}]))
   (test/testing "Many events with low metric."
     (riemann-test/test-stream
      (load-indicator "test-service" 10 60 (fn [res] (fn [_metric-event] (test/is (false? res)))))
-     [{:service "test-service" :metric 1}
-      {:service "test-service" :metric 10}
-      {:service "test-service" :metric 10}]
-     [{:service "test-service" :metric 1}
-      {:service "test-service" :metric 11/2}
-      {:service "test-service" :metric 7}])))
+     [{:service "test-service" :host common/hostname :metric 1}
+      {:service "test-service" :host common/hostname :metric 10}
+      {:service "test-service" :host common/hostname :metric 10}]
+     [{:service "test-service" :host common/hostname :metric 1}
+      {:service "test-service" :host common/hostname :metric 11/2}
+      {:service "test-service" :host common/hostname :metric 7}])))
 
 (test/deftest t-riemann-load-indicator
   (test/testing "One event with high metric."
     (riemann-test/run-stream
-     (riemann-load-indicator load-atom :test-level 1 prn)
-     [{:service riemann-netty-event-executor-queue-size-service-name :metric (* riemann-netty-event-executor-queue-size-load-limit 2)}])
+     (riemann-load-indicator load-atom :test-level 1)
+     [{:service riemann-netty-event-executor-queue-size-service-name :host common/hostname :metric (* riemann-netty-event-executor-queue-size-load-limit 2)}])
     (test/is (true? ((load-level-hit-fn? load-atom :test-level)))))
   (test/testing "One event with low metric."
     (riemann-test/run-stream
      (riemann-load-indicator load-atom :test-level 1)
-     [{:service riemann-netty-event-executor-queue-size-service-name :metric (/ riemann-netty-event-executor-queue-size-load-limit 2)}])
+     [{:service riemann-netty-event-executor-queue-size-service-name :host common/hostname :metric (/ riemann-netty-event-executor-queue-size-load-limit 2)}])
     (test/is (false? ((load-level-hit-fn? load-atom :test-level)))))
   (test/testing "Many events with high metric."
     (riemann-test/run-stream
      (riemann-load-indicator load-atom :test-level 1)
-     [{:service riemann-netty-event-executor-queue-size-service-name :metric (* riemann-netty-event-executor-queue-size-load-limit 2)}
-      {:service riemann-netty-event-executor-queue-size-service-name :metric (* riemann-netty-event-executor-queue-size-load-limit 2)}
-      {:service riemann-netty-event-executor-queue-size-service-name :metric (/ riemann-netty-event-executor-queue-size-load-limit 2)}])
+     [{:service riemann-netty-event-executor-queue-size-service-name :host common/hostname :metric (* riemann-netty-event-executor-queue-size-load-limit 2)}
+      {:service riemann-netty-event-executor-queue-size-service-name :host common/hostname :metric (* riemann-netty-event-executor-queue-size-load-limit 2)}
+      {:service riemann-netty-event-executor-queue-size-service-name :host common/hostname :metric (/ riemann-netty-event-executor-queue-size-load-limit 2)}])
     (test/is (true? ((load-level-hit-fn? load-atom :test-level)))))
   (test/testing "Many events with low metric."
     (riemann-test/run-stream
      (riemann-load-indicator load-atom :test-level 1)
-     [{:service riemann-netty-event-executor-queue-size-service-name :metric (* riemann-netty-event-executor-queue-size-load-limit 2)}
-      {:service riemann-netty-event-executor-queue-size-service-name :metric (/ riemann-netty-event-executor-queue-size-load-limit 2)}
-      {:service riemann-netty-event-executor-queue-size-service-name :metric (/ riemann-netty-event-executor-queue-size-load-limit 2)}
-      {:service riemann-netty-event-executor-queue-size-service-name :metric (/ riemann-netty-event-executor-queue-size-load-limit 2)}])
+     [{:service riemann-netty-event-executor-queue-size-service-name :host common/hostname :metric (* riemann-netty-event-executor-queue-size-load-limit 2)}
+      {:service riemann-netty-event-executor-queue-size-service-name :host common/hostname :metric (/ riemann-netty-event-executor-queue-size-load-limit 2)}
+      {:service riemann-netty-event-executor-queue-size-service-name :host common/hostname :metric (/ riemann-netty-event-executor-queue-size-load-limit 2)}
+      {:service riemann-netty-event-executor-queue-size-service-name :host common/hostname :metric (/ riemann-netty-event-executor-queue-size-load-limit 2)}])
     (test/is (false? ((load-level-hit-fn? load-atom :test-level))))))
 
 (test/deftest t-with-riemann-circuit-breaker
@@ -102,9 +103,9 @@
     (test/testing "indicator stream"
       (riemann-test/run-stream
        (indicator-stream)
-       [{:service riemann-netty-event-executor-queue-size-service-name :metric (* riemann-netty-event-executor-queue-size-load-limit 2)}
-        {:service riemann-netty-event-executor-queue-size-service-name :metric (* riemann-netty-event-executor-queue-size-load-limit 2)}
-        {:service riemann-netty-event-executor-queue-size-service-name :metric (/ riemann-netty-event-executor-queue-size-load-limit 2)}])
+       [{:service riemann-netty-event-executor-queue-size-service-name :host common/hostname :metric (* riemann-netty-event-executor-queue-size-load-limit 2)}
+        {:service riemann-netty-event-executor-queue-size-service-name :host common/hostname :metric (* riemann-netty-event-executor-queue-size-load-limit 2)}
+        {:service riemann-netty-event-executor-queue-size-service-name :host common/hostname :metric (/ riemann-netty-event-executor-queue-size-load-limit 2)}])
       (test/is (true? ((load-level-hit-fn? load-atom :t-make-indicator-stream-and-breaker-stream)))))
     (test/testing "breaker stream"
       (riemann-test/test-stream
@@ -117,9 +118,9 @@
   (test/testing "indicator stream"
     (riemann-test/run-stream
      (riemann-streams/sdo (indicator-stream))
-     [{:service riemann-netty-event-executor-queue-size-service-name :metric (* riemann-netty-event-executor-queue-size-load-limit 2)}
-      {:service riemann-netty-event-executor-queue-size-service-name :metric (* riemann-netty-event-executor-queue-size-load-limit 2)}
-      {:service riemann-netty-event-executor-queue-size-service-name :metric (/ riemann-netty-event-executor-queue-size-load-limit 2)}])
+     [{:service riemann-netty-event-executor-queue-size-service-name :host common/hostname :metric (* riemann-netty-event-executor-queue-size-load-limit 2)}
+      {:service riemann-netty-event-executor-queue-size-service-name :host common/hostname :metric (* riemann-netty-event-executor-queue-size-load-limit 2)}
+      {:service riemann-netty-event-executor-queue-size-service-name :host common/hostname :metric (/ riemann-netty-event-executor-queue-size-load-limit 2)}])
     (test/is (true? ((load-level-hit-fn? load-atom :t-define-breaker)))))
   (test/testing "breaker stream"
     (riemann-test/test-stream
