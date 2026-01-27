682de3f4850b3ad1f81d60d233db69e1c87d743b
Christina Zeller <christina.zeller@active-group.de>
2026-01-27 08:43:33 +0100

    index-test: refactore, reorganise tests

--- test/active_riemann/index_test.clj
+++ test/active_riemann/index_test.clj
@@ -3,7 +3,6 @@
              [clojure.edn :as clojure-edn]
              [riemann.config :as riemann-config]
              [riemann.index :as riemann-index]
-             [riemann.test :as riemann-test]
              [active.clojure.logger.internal :as internal]
              [active-riemann.index :as active-index]
              [test-with-files.tools :refer [with-tmp-dir]]
@@ -42,97 +41,146 @@
 (defn file-path [file-name]
   (str "test/resources/" file-name))
 
-(riemann-test/with-test-env
-  (t/deftest t-restore-index
-    (let [the-index (riemann-config/index)]
+(t/deftest t-restore-index-no-edn-file
+  (active-index/restore-index (riemann-config/index) "not-existing.edn")
+  (t/is (= [{:level :error,
+             :message
+             ["active-riemann.index/try-open-edn riemann-index backup file not found: not-existing.edn"]}]
+           (captured-events))))
 
-      (t/is (= 0 (count (riemann.index/search the-index "*"))))
-      (t/is (= [] (captured-events)))
+;; restore index
 
-      (active-index/restore-index the-index "not-existing.edn")
+(t/deftest t-restore-index-empty-edn-file
+  (let [the-index (riemann-config/index)]
+    (t/is (= [] (riemann.index/search the-index "*")))
+    (t/is (= [] (captured-events)))
 
-      (t/is (= 0 (count (riemann.index/search the-index "*"))))
-      (t/is (= [{:level :error,
+    (active-index/restore-index the-index (file-path "empty.edn"))
+
+    ;; nothing added to the index
+    (t/is (= [] (riemann.index/search the-index "*")))
+    (t/is (= [] (captured-events)))
+
+    (riemann-index/insert the-index {:host "the-host" :service "baz" :time 124})
+    (t/is (= [{:host "the-host" :service "baz" :time 124}]
+             (riemann.index/search the-index "*")))
+
+    ;; nothing added to the index and nothing existing overwritten in the index
+    (active-index/restore-index the-index (file-path "empty.edn"))
+
+    (t/is (= [{:host "the-host" :service "baz" :time 124}]
+             (riemann.index/search the-index "*")))
+    (t/is (= [] (captured-events)))))
+
+(t/deftest t-restore-index-invalid-edn-file
+  (let [the-index (riemann-config/index)]
+    (active-index/restore-index the-index (file-path "invalid-content.edn"))
+
+    ;; nothing added to the index
+    (t/is (= [] (riemann.index/search the-index "*")))
+    (t/is (= [{:level :error,
+               :message
+               ["active-riemann.index/try-open-edn Error reading riemann-index backup file: test/resources/invalid-content.edn Invalid number: +1j"]}]
+             (captured-events)))
+    (reset! *captured-events* [])
+    (t/is (= [] (captured-events)))
+
+    (riemann-index/insert the-index {:host "the-host" :service "baz" :time 124})
+    (t/is (= [{:host "the-host" :service "baz" :time 124}]
+             (riemann.index/search the-index "*")))
+
+    ;; nothing added to the index and nothing existing overwritten in the index
+    (active-index/restore-index the-index (file-path "invalid-content.edn"))
+
+    (t/is (= [{:host "the-host" :service "baz" :time 124}]
+             (riemann.index/search the-index "*")))
+    (t/is (= [{:level :error,
+               :message
+               ["active-riemann.index/try-open-edn Error reading riemann-index backup file: test/resources/invalid-content.edn Invalid number: +1j"]}]
+             (captured-events)))))
+
+(t/deftest t-restore-index-edn-file-with-content
+  (let [the-index (riemann-config/index)]
+    (riemann-index/insert the-index {:host "the-host" :service "baz" :time 124})
+    (active-index/restore-index the-index (file-path "riemann-index-backup.edn"))
+    (riemann-index/insert the-index {:host "the-host" :service "biz" :time 125})
+
+      ;; TODO: is there a logical order in this result?
+    (t/is (= [{:host "the-host" :service "biz" :time 125}
+              {:host "localhost" :service "foo" :metric 10 :time 123}
+              {:host "the-host" :service "baz" :time 124}
+              {:host "localhost" :service "bar" :metric 10 :time 123}]
+             (riemann.index/search the-index "*")))
+    (t/is (= [] (captured-events)))))
+
+;; backup-index
+
+(t/deftest t-backup-index-no-edn-file-path
+  (let [the-index (riemann-config/index)]
+    (mapv #(riemann-index/insert the-index %)
+          [{:host "the-host" :service "biz" :time 125}
+           {:host "localhost" :service "foo" :metric 10 :time 123}
+           {:host "the-host" :service "baz" :time 124}
+           {:host "localhost" :service "bar" :metric 10 :time 123}])
+
+    (t/is (= [{:host "the-host" :service "biz" :time 125}
+              {:host "localhost" :service "foo" :metric 10 :time 123}
+              {:host "the-host" :service "baz" :time 124}
+              {:host "localhost" :service "bar" :metric 10 :time 123}]
+             (riemann.index/search the-index "*")))
+
+    (active-index/backup-index the-index nil)
+    (t/is (= [{:level :error,
+               :message
+               ["active-riemann.index/try-store-edn no riemann-index backup file specified: nil"]}]
+             (captured-events)))))
+
+;; valid-file-path: file does not exist: creates file && file contains the four metrics
+(t/deftest t-backup-index-to-new-file
+  (with-tmp-dir tmp-dir
+    (let [the-index (riemann-config/index)
+          file-path (str tmp-dir "riemann-index-backup.edn")]
+      (mapv #(riemann-index/insert the-index %)
+            [{:host "the-host" :service "biz" :time 125}
+             {:host "localhost" :service "foo" :metric 10 :time 123}
+             {:host "the-host" :service "baz" :time 124}
+             {:host "localhost" :service "bar" :metric 10 :time 123}])
+
+      (active-index/backup-index the-index file-path)
+
+      (t/is (= [{:level :info,
                  :message
-                 ["active-riemann.index/try-open-edn riemann-index backup file not found: not-existing.edn"]}]
+                 ["active-riemann.index/try-store-edn Successfully stored riemann-index backup file:"]}]
                (captured-events)))
-      (reset! *captured-events* [])
-      (t/is (= [] (captured-events)))
-
-      (active-index/restore-index the-index (file-path "empty.edn"))
-
-      (t/is (= 0 (count (riemann.index/search the-index "*"))))
-      (t/is (= [] (captured-events)))
-
-      (active-index/restore-index the-index (file-path "invalid-content.edn"))
-
-      (t/is (= 0 (count (riemann.index/search the-index "*"))))
-      (t/is (= [{:level :error,
-                 :message
-                 ["active-riemann.index/try-open-edn Error reading riemann-index backup file: test/resources/invalid-content.edn Invalid number: +1j"]}]
-               (captured-events)))
-      (reset! *captured-events* [])
-      (t/is (= [] (captured-events)))
-
-      (riemann-index/insert the-index {:host "the-host" :service "baz" :time 124})
-      (active-index/restore-index the-index (file-path "riemann-index-backup.edn"))
-      (riemann-index/insert the-index {:host "the-host" :service "biz" :time 125})
-
-      (t/is (= 4 (count (riemann.index/search the-index "*"))))
-        ;; TODO: is there a logical order in this result?
       (t/is (= [{:host "the-host" :service "biz" :time 125}
-                {:host "localhost" :service "foo" :metric 10, :time 123}
+                {:host "localhost", :service "foo", :metric 10, :time 123}
                 {:host "the-host" :service "baz" :time 124}
-                {:host "localhost" :service "bar" :metric 10, :time 123}]
-               (riemann.index/search the-index "*")))
-      (t/is (= [] (captured-events))))))
+                {:host "localhost", :service "bar", :metric 10, :time 123}]
+               (clojure-edn/read-string (slurp file-path)))))))
 
-(riemann-test/with-test-env
-  (t/deftest t-backup-index
-    (with-tmp-dir tmp-dir
-      (let [the-index (riemann-config/index)]
-        (mapv #(riemann-index/insert the-index %)
-              [{:host "the-host" :service "biz" :time 125}
-               {:host "localhost", :service "foo", :metric 10, :time 123}
-               {:host "the-host" :service "baz" :time 124}
-               {:host "localhost", :service "bar", :metric 10, :time 123}])
+;; valid-file-path: file does exist: overwrite file && file contains the four metrics
+(t/deftest t-backup-index-to-existing-file
+  (with-tmp-dir tmp-dir
+    (let [the-index (riemann-config/index)
+          file-path (str tmp-dir "riemann-index-backup.edn")]
+      (mapv #(riemann-index/insert the-index %)
+            [{:host "the-host" :service "biz" :time 125}
+             {:host "localhost" :service "foo" :metric 10 :time 123}
+             {:host "the-host" :service "baz" :time 124}
+             {:host "localhost" :service "bar" :metric 10 :time 123}])
 
-        (t/is (= 4 (count (riemann.index/search the-index "*"))))
-        (t/is (= [] (captured-events)))
+      (spit file-path ["hello" "world"])
+      (t/is (= ["hello" "world"]
+               (clojure-edn/read-string (slurp file-path))))
 
-        (active-index/backup-index the-index nil)
-        (t/is (= [{:level :error,
-                   :message
-                   ["active-riemann.index/try-store-edn no riemann-index backup file specified: nil"]}]
-                 (captured-events)))
-        (reset! *captured-events* [])
-        (t/is (= [] (captured-events)))
+      (active-index/backup-index the-index file-path)
 
-          ;; valid-file-path: file does not exist: creates file && file contains the four metrics
-        (active-index/backup-index the-index (str tmp-dir "riemann-index-backup.edn"))
-        (t/is (= [{:level :info,
-                   :message
-                   ["active-riemann.index/try-store-edn Successfully stored riemann-index backup file:"]}]
-                 (captured-events)))
-        (t/is (= [{:host "the-host" :service "biz" :time 125}
-                  {:host "localhost", :service "foo", :metric 10, :time 123}
-                  {:host "the-host" :service "baz" :time 124}
-                  {:host "localhost", :service "bar", :metric 10, :time 123}]
-                 (clojure-edn/read-string (slurp (str tmp-dir "riemann-index-backup.edn")))))
-        (reset! *captured-events* [])
-        (t/is (= [] (captured-events)))
-
-          ;; valid-file-path: file does exist: overwrite file && file contains the four metrics
-        (spit (str tmp-dir "riemann-index-backup.edn") ["hello" "world"] :append false)
-        (t/is (= ["hello" "world"]
-                 (clojure-edn/read-string (slurp (str tmp-dir "riemann-index-backup.edn")))))
-        (active-index/backup-index the-index (str tmp-dir "riemann-index-backup.edn"))
-        (t/is (= [{:level :info,
-                   :message
-                   ["active-riemann.index/try-store-edn Successfully stored riemann-index backup file:"]}]
-                 (captured-events)))
-        (t/is (= [{:host "the-host" :service "biz" :time 125}
-                  {:host "localhost", :service "foo", :metric 10, :time 123}
-                  {:host "the-host" :service "baz" :time 124}
-                  {:host "localhost", :service "bar", :metric 10, :time 123}]
-                 (clojure-edn/read-string (slurp (str tmp-dir "riemann-index-backup.edn")))))))))
+      (t/is (= [{:level :info,
+                 :message
+                 ["active-riemann.index/try-store-edn Successfully stored riemann-index backup file:"]}]
+               (captured-events)))
+      (t/is (= [{:host "the-host" :service "biz" :time 125}
+                {:host "localhost", :service "foo", :metric 10, :time 123}
+                {:host "the-host" :service "baz" :time 124}
+                {:host "localhost", :service "bar", :metric 10, :time 123}]
+               (clojure-edn/read-string (slurp file-path)))))))
